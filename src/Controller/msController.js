a691dab51d465cd54a410d44cf35268023ce4c98
LoniMaple <joshu4drake@gmail.com>
2022-12-16 21:46:47 -0300

    a

--- src/Controller/msController.js
+++ src/Controller/msController.js
@@ -1,7 +1,7 @@
 const MS_Schema = require("../Models/MS_Schema");
 
 //modules
-const gsm = require("../Modules/greetingsSearchModule");
+const gsm = require("../modules/greetingsSearchModule");
 
 let id;
 let filteredData;
